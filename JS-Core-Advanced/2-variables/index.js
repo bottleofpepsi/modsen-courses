be355a02d34c118bd3df9cbd93313f05866d8996
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-12 19:34:51 +0300

    add solution to second task

---
+++ JS-Core-Advanced/2-variables/index.js
@@ -0,0 +1,18 @@
+/*
+Напишите код, в котором попытаетесь обратиться к переменной до её объявления с использованием var, let и const. 
+Определите, какие переменные допускают такое использование, а какие нет.
+*/
+
+
+//переменная, создаваемая с помощью var допускает обращение к себе до объявления, 
+//но её значение будет undefined 
+console.log(varVariable);
+var varVariable = "var";
+
+//ошибка ReferenceError: Cannot access 'letVariable' before initialization
+console.log(letVariable);
+let letVariable = "let";
+
+//ошибка ReferenceError: Cannot access 'constVariable' before initialization
+console.log(constVariable);
+const constVariable = "const";
