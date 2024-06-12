83e7555bb76cbde79a9d3cb183c1e04bd9816abe
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-12 20:17:37 +0300

    add solution to fourth task

---
+++ JS-Core-Advanced/4-arrays/index.js
@@ -0,0 +1,13 @@
+/*
+Напишите функцию, которая принимает массив с числами 
+и находит сумму квадратов элементов этого массива.
+*/
+
+function sumOfSecondPowers(array) {
+    return array.reduce((acc, x) => acc + x * x, 0)
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+//1 + 4 + 9 + 16 + 25 = 55
+console.log(sumOfSecondPowers(numbers));
